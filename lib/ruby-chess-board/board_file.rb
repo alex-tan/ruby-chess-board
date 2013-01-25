c0f6ec18c6758b4e4a483894103959e0b0e52e3d
Alex Tan <alex.r.tan@gmail.com>
2013-01-24 22:47:47 -0600

    Add specs and documentation for BoardFile. Make it responsible for returning EmptySquares.

--- lib/ruby-chess-board/board_file.rb
+++ lib/ruby-chess-board/board_file.rb
@@ -1,23 +1,48 @@
 module RubyChessBoard
+  # Represents a single file made up of 8 ranks.
   class BoardFile
-    attr_reader :ranks
+    # Instances of this class are used to represent an empty square.
+    EmptySquare = Class.new
 
+    include Enumerable
+
+    attr_reader :ranks
+    
     def initialize
       @ranks = Array.new(8)
     end
-
-    def rank(rank_number)
-      rank_index = rank_index(rank_number)
-      ranks[rank_index]
+    
+    # Returns true if any of its ranks contain a piece.
+    # @return [Boolean]
+    def has_piece?(piece)
+      find { |rank| rank == piece }
     end
-
+    
+    # Yields the value of each of the ranks, starting with 1,
+    # up through 8.
+    # @yield [rank]
+    def each
+      ranks.each { |rank| yield rank }
+    end
+    
+    # Returns either a piece if there is something at a square.
+    # Returns an instance of {EmptySquare} otherwise.
+    # @return [Piece, EmptySquare]
+    def at_rank(rank_number)
+      rank_index = rank_index(rank_number)
+      ranks[rank_index] || EmptySquare.new
+    end
+    
+    # Given a rank number and a value, sets the rank.
+    # @return [void]
     def set_rank(rank_number, value)
       rank_index = rank_index(rank_number) 
       @ranks[rank_index] = value
     end
 
     private
-
+    
+    # Converts a rank to an array number.
     def rank_index(rank_number)
       rank_number.to_i - 1
     end
---
+++ spec/ruby-chess-board/board_file_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+module RubyChessBoard
+  describe BoardFile do
+    subject(:file) { BoardFile.new }
+
+    it "is made up of 8 ranks" do
+      expect(file.ranks.size).to eq(8)
+    end
+
+    describe "#each" do
+      it "yields each of the ranks" do
+        expected = (1..8).to_a
+
+        expected.each do |number|
+          file.set_rank(number, number)
+        end
+
+        result = []
+
+        file.each { |rank| result << rank }
+
+        expect(result).to eq(expected)
+      end
+    end
+
+    describe "#has_piece?" do
+      let(:piece) { Pawn.new(color: :white, starting_position: :a1) }
+
+      context "when one of its ranks contains a piece" do
+        it 'is true' do
+          file.set_rank(1, piece)
+          expect(file).to have_piece(piece)
+        end
+      end
+
+      context "when none of its ranks contain a piece" do
+        it 'is false' do
+          expect(file).to_not have_piece(piece)
+        end
+      end
+    end
+
+    describe "#at_rank" do
+      context "when there is something there" do
+        it "returns what is at the rank number" do
+          piece = build(:pawn)
+
+          file.set_rank(4, piece)
+
+          expect(file.at_rank(4)).to eq(piece)
+        end
+      end
+
+      context "when there is nothing there" do
+        it "returns an instance of EmptySquare" do
+          expect(file.at_rank(4)).to be_kind_of(BoardFile::EmptySquare)
+        end
+      end
+    end
+
+    describe "#set_rank" do
+      it "sets a rank to the value provided" do
+        piece = build(:pawn)
+        expect { 
+          file.set_rank(5, piece)
+        }.to change { file.at_rank(5) }.to(piece)
+      end
+    end
+  end
+end
