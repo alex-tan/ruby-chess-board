8c25c14249a33b2c5b623543b8441b37183beb4c
Alex Tan <alex.r.tan@gmail.com>
2013-01-25 18:53:35 -0600

    Format spec docstrings.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,10 +8,11 @@ require_all 'spec/support',
             'spec/ruby-chess-board/shared_examples'
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
   config.include BoardHelpers
+  config.include FactoryGirl::Syntax::Methods
 
   config.expect_with(:rspec) { |c| c.syntax = :expect } 
+  config.format_docstrings   { |ds| ds.strip.gsub(/\s+/, ' ') }
   config.mock_with :mocha
   config.order = :random
 end
