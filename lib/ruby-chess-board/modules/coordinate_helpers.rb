6014c7d3bafb8e4cf52e27ae4f9986ab7ca4b4fe
Alex Tan <alex.r.tan@gmail.com>
2013-01-29 13:44:21 -0600

    CoordinateHelpers #empty_square? and documentation.

--- lib/ruby-chess-board/modules/coordinate_helpers.rb
+++ lib/ruby-chess-board/modules/coordinate_helpers.rb
@@ -1,8 +1,15 @@
 module RubyChessBoard
   module CoordinateHelpers
-    
     private
-
+    
+    # Returns true if the variable is an empty square.
+    # @return [Boolean]
+    def empty_square?(variable)
+      variable.kind_of?(BoardFile::EmptySquare)
+    end
+    
+    # Returns true if the variable is an impossible coordinate.
+    # @return [Boolean]
     def impossible_coordinate?(variable)
       variable.kind_of?(BoardCoordinate::ImpossibleCoordinate)
     end
