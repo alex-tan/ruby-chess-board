6133b0630d10b754258607070d41ff9f294927a9
Alex Tan <alex.r.tan@gmail.com>
2013-01-29 18:09:27 -0600

    Change BoardHelper method names.

--- spec/support/helpers/board_helpers.rb
+++ spec/support/helpers/board_helpers.rb
@@ -1,8 +1,16 @@
 module BoardHelpers
-  def coordinates(*coordinates)
-    coordinates.map { |c| RubyChessBoard::BoardCoordinate.new(c) }
+  # Return an array of coordinates.
+  def coordinate_array(*coordinates)
+    coordinates.map { |c| coordinate(c) }
   end
 
+  # Return a CoordinateSet
+  def coordinate_set(*coordinates)
+    coords = coordinate_array(*coordinates)
+    build(:coordinate_set, coordinates: coords)
+  end
+  
+  # Return a single coordinate.
   def coordinate(coordinate)
     RubyChessBoard::BoardCoordinate.new(coordinate)
   end
