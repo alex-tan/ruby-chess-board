7bab6e80ee3acf0b629fb2d2b0f91117724a084b
Alex Tan <alex.r.tan@gmail.com>
2013-01-30 20:51:17 -0600

    BoardCoordinate factories: Allow initialization with x & y, file & rank.

--- spec/factories/board_coordinates.rb
+++ spec/factories/board_coordinates.rb
@@ -1,7 +1,21 @@
 FactoryGirl.define do
   factory :board_coordinate, class: RubyChessBoard::BoardCoordinate do
-    square_name :a1
+    ignore do
+      square_name :a1
+      x nil
+      y nil
+      file nil
+      rank nil
+    end
 
-    initialize_with { new(square_name) }
+    initialize_with do
+      if x && y
+        new(x, y)
+      elsif file && rank
+        new(file, rank)
+      else
+        new(square_name)
+      end
+    end
   end
 end
