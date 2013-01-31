626a1d1f3b3e566e99240c21a4d854c54210c58b
Alex Tan <alex.r.tan@gmail.com>
2013-01-30 18:28:55 -0600

    EmptySquare spec - use empty square factory.

--- spec/ruby-chess-board/board_file/empty_square_spec.rb
+++ spec/ruby-chess-board/board_file/empty_square_spec.rb
@@ -2,18 +2,16 @@ require 'spec_helper'
 
 module RubyChessBoard
   describe BoardFile::EmptySquare do
-    subject(:square) { BoardFile::EmptySquare.new }
+    subject(:square) { build(:empty_square) }
 
     describe "equality testing" do
       it "is equal to any other empty square" do
-        square_2 = BoardFile::EmptySquare.new
-
+        square_2 = build(:empty_square) 
         expect(square).to eq(square_2)
       end
 
       it "is not equal to other pieces" do
         pawn = build(:pawn)
-
         expect(square).to_not eq(pawn)
       end
     end
